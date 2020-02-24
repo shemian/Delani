87ae8cd725f5d4af99ed96ef3dbb83515a9688e2
ian <shemian092@gmail.com>
2020-02-24 12:29:14 +0300

    add this

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
-function click() {
-    var name=document.getElementById("name")
-    var emai=document.getElementById("email")
-    
-}
+function clickThis(name) {
+    var name = document.getElementById("name").value;
+    if(name !== null){
+        alert(name + " we've recieved your message.we will check on you later.Thank you"); 
+    };
+    return name;
+};
